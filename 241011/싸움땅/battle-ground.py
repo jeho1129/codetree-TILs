0af415a1e55ddb2379fa799691b0e92e2efafd82
Jiho Lee <89183883+jeho1129@users.noreply.github.com>
2024-10-11 00:34:09 +0900

    Title: 싸움땅, Time: 197ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241011/싸움땅/battle-ground.py
+++ 241011/싸움땅/battle-ground.py
@@ -10,7 +10,7 @@ def game(n):
         new_y, new_x = player[n][0] + dy[player[n][2]], player[n][1] + dx[player[n][2]]
 
     player[n][0], player[n][1] = new_y, new_x
-    # 이동한 방향에 플레이어가 없으면 총이 있는 지 확인
+    # 이동한 방향에 플레이어가 있는 지 확인
     for i in range(M):
         if i == n:
             continue
@@ -23,11 +23,14 @@ def game(n):
         x = player[n][4]
         y = guns[new_y][new_x]
         # 총이 있는 지 확인
-        if y > 0:
+        if y:
             # 플레이어가 총이 없거나, 기존 공격력이 낮을 경우
-            if 0 <= x < y:
-                player[n][4] = y
-                guns[new_y][new_x] = x
+            if 0 <= x < max(y):
+                change = guns[new_y][new_x].pop()
+                player[n][4] = change
+                if 0 < x:
+                    guns[new_y][new_x].append(x)
+                    guns[new_y][new_x].sort()
 
 
 def fight(a, b, new_y, new_x):
@@ -52,15 +55,20 @@ def fight(a, b, new_y, new_x):
     points[winner] += abs((a1 + a2) - (b1 + b2))
     # 진 플레이어는 본인이 가지고 있는 총을 해당 격자에 내려 놓고
     x = player[loser][4]
-    y = guns[new_y][new_x]
-    if x > y:
-        guns[new_y][new_x] = x
+    if x > 0:
+        guns[new_y][new_x].append(x)
+        guns[new_y][new_x].sort()
+
     player[loser][4] = 0
     # 이긴 플레이어는 승리한 칸에서 떨어져 있는 총과 원래 들고 있는 총 중 가장 공격력이 높은 총을 획득하고, 나머지 총은 내려 놓는다
     x = player[winner][4]
-    if x < y:
-        guns[new_y][new_x] = x
-        player[winner][4] = y
+    if guns[new_y][new_x]:
+        if x < max(guns[new_y][new_x]):
+            y = guns[new_y][new_x].pop()
+            player[winner][4] = y
+            if x > 0:
+                guns[new_y][new_x].append(x)
+                guns[new_y][new_x].sort()
     # 진 플레이어는 해당 플레이어가 원래 가고 있던 방향으로 한 칸 더 이동
     y1, x1 = player[loser][0] + dy[player[loser][2]], player[loser][1] + dx[player[loser][2]]
     # 범위 밖인 경우, 다른 플레이어가 이미 위치해 있는 경우
@@ -77,19 +85,20 @@ def fight(a, b, new_y, new_x):
             else:
                 break
     player[loser][0], player[loser][1] = y1, x1
-    # 해당 칸에 총이 있다면 다시 비교
-    x2 = player[loser][4]
     y2 = guns[y1][x1]
     # 총이 있는 지 확인
-    if y2 > 0:
-        # 플레이어가 총이 없거나, 기존 공격력이 낮을 경우
-        if 0 <= x2 < y2:
-            player[loser][4] = y2
-            guns[y1][x1] = x2
+    if y2:
+        change = guns[y1][x1].pop()
+        player[loser][4] = change
 
 
 N, M, K = map(int, input().split())  # N = 격자의 크기, M = 플레이어 수, K = 라운드
-guns = [list(map(int, input().split())) for _ in range(N)]
+info = [list(map(int, input().split())) for _ in range(N)]
+guns = [[[]] * N for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        if info[i][j] != 0:
+            guns[i][j] = [info[i][j]]
 # [0] = x 좌표, [1] = y 좌표, [2] = 방향(상우하좌), [3] = 초기 능력치, [4] = 보유 공격력
 player = [list(map(int, input().split())) + [0] for _ in range(M)]
 for i in range(M):
